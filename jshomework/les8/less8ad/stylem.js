ba351afa119741cd775312586ff87d4ab40b3bdd
Sofi-Sh <sonia.sh13f@gmail.com>
2022-05-13 23:02:47 +0300

    les8ad

---
+++ jshomework/les8/less8ad/stylem.js
@@ -0,0 +1,12 @@
+// написати рекурсивну функцію,
+// яка збирає всі назви класів з файлу rules.html
+// в окремий масив. масив вивести в консоль
+
+// function first(start) {
+//     console.log(start);
+//     let children= start.children;
+//     for (const child of children) {
+//         first(child);
+//     }
+// }
+// first(document.body)
